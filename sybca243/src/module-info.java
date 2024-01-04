ac35e226f1cf2c9b2db8315ebcca2f3c355b0074
stania rebelo <staniarebelo08@gmail.com>
2024-01-04 10:27:54 +0530

    comit

---
+++ sybca243/src/module-info.java
@@ -0,0 +1,2 @@
+module sybca243 {
+}
---
+++ sybca243/src/pkgbca243/democlass.java
@@ -0,0 +1,11 @@
+package pkgbca243;
+
+public class democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("github hello world");
+	}
+
+}
