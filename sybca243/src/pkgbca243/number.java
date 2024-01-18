2174bb9d3ef1af93c4b2d74c68417e4db710d728
stania rebelo <staniarebelo08@gmail.com>
2024-01-18 10:23:13 +0530

    fhtgj

---
+++ sybca243/src/pkgbca243/number.java
@@ -0,0 +1,17 @@
+package pkgbca243;
+
+public class number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 2;
+		int b = 3;
+		int sum = a+b;
+		int sub = a-b;
+		System.out.println("the sum of two number: "+sum);
+
+		System.out.println("the difference of two number: "+sub);
+	}
+
+}
